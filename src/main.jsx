422dacb4bf7a7b633639b4d12020e7134a2c56f3
AmirHossein Ebadi F. Azar <Amir.ebadi93@hotmail.com>
2025-05-31 17:30:08 +0330

    Lupon Persian

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-import English from './English.jsx'
-import './English.css'
+import Persian from './Persian.jsx'
+import './Persian.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <English />
+    <Persian />
   </StrictMode>,
 )
